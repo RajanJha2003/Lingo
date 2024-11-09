e65f88107d3fbfdaa8abb75dbf6ae89840685a65
Rajan <rajanj448@student.sfit.ac.in>
2024-11-09 20:36:31 +0530

    Landing page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,14 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+
+import type { Metadata, Viewport } from "next";
+import { Nunito } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const font = Nunito({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  themeColor: "#22C55E",
+};
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+       <body className={font.className}>
         {children}
       </body>
     </html>
